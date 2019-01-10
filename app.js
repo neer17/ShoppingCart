74ef26686604fa41a4dcd9ceb956d5e78108aea7
Neeraj Sewani <neerajsewanisrt8@gmail.com>
2019-01-10 12:55:49 +0530

    lesson 14 completed

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var mongoose = require('mongoose')
 var flash = require('connect-flash')
 var passport = require('passport')
 var validator = require('express-validator')
+var MongooseStore = require('connect-mongo')(session)
 
-mongoose.connect('mongodb://localhost/shopping', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/shopping', {
+  useNewUrlParser: true
+})
 
 //  loading the "index" route
 var indexRouter = require('./routes/index')
@@ -22,16 +25,31 @@ require('./config/passport-config')
 var app = express()
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.engine('.hbs', expressHbs({
+  defaultLayout: 'layout',
+  extname: '.hbs'
+}))
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs')
 
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+  extended: true
+}))
 app.use(validator())
 app.use(cookieParser())
-app.use(session({secret: 'mysuperseccret', resave: false, saveUninitialized: false}))
+app.use(session({
+  secret: 'mysuperseccret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongooseStore({
+    'mongooseConnection': mongoose.connection
+  }),
+  cookie: {
+    maxAge: 180 * 60 * 1000
+  }
+}))
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
@@ -39,8 +57,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //  checking if the user is logged in
+//  storing session and logged in info in global variables
 app.use((req, res, next) => {
   res.locals.isLoggedIn = req.isAuthenticated()
+  res.locals.session = req.session
   next()
 })
 
@@ -49,12 +69,12 @@ app.use('/', indexRouter)
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require('passport')
 
 var Product = require('./../models/product')
+var Cart = require('./../models/cart')
 
 /* home page. */
 router.get('/', function (req, res, next) {
@@ -14,6 +15,32 @@ router.get('/', function (req, res, next) {
 
     res.render('shop/index', { title: 'Shopping Cart', products: docs })
   })
+})
+
+//  add to cart route
+router.get('/add-to-cart/:id', (req, res) => {
+  var id = req.params.id
+
+  var cart = new Cart(req.session.cart? req.session.cart: {})
+
+  Product.findById(id).then((product) => {
+    cart.add(product, product.id)
+
+    //  setting the whole cart in the session
+    req.session.cart = cart
+
+    console.log(req.session.cart)
+
+    res.send('product page')
+  }).catch((err) => {
+    console.log(err)
+    res.redirect('/')
+  })
+})
+
+//  shooping cart route
+router.get('/shopping-cart', (req, res, next) => {
+  res.render('shop/shopping_cart', {cart: req.session.cart})
 })
 
 module.exports = router
